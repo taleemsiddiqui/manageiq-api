473dcab68894b0401322d124c17ece033ee94d12
tariq siddiqui <tariq.siddiqui@systemsltd.com>
2020-06-12 16:48:07 +0500

    restore changes

--- app/controllers/api/coffees_controller.rb
+++ app/controllers/api/coffees_controller.rb
@@ -1,7 +1,13 @@
 module Api
   class CoffeeController < ActionController::API
     def index
-      render :plain => 'coffee'
+      url = URI.parse('http://34.67.132.139:9000/aws/resources/regions')
+            req = Net::HTTP::Get.new(url.to_s)
+            res = Net::HTTP.start(url.host, url.port) {|http|
+            http.request(req)
+            }
+            
+            render :json => {message: res.body}
     end
   end
 end
