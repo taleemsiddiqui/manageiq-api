a1dbfc8ebd634be0e45dd9ea05f6ccc351e37be0
tariq siddiqui <tariq.siddiqui@systemsltd.com>
2020-06-17 20:34:05 +0500

    change base url and port

--- app/controllers/api/coffees_controller.rb
+++ app/controllers/api/coffees_controller.rb
@@ -1,11 +1,13 @@
 module Api
   class CoffeesController < ActionController::API
 
-    URL = "https://cloudmanagement.me"
+    #URL = "http://cloudmanagement.me"
+    URL = "http://localhost"
     
     def index
 
-      path = "#{URL}:9000/#{params[:provider]}/#{params[:entity]}/#{params[:endpoint]}"
+      #path = "#{URL}:9000/#{params[:provider]}/#{params[:entity]}/#{params[:endpoint]}"
+      path = "#{URL}:6000/#{params[:provider]}/#{params[:entity]}/#{params[:endpoint]}"
       url = URI.parse(path)
             req = Net::HTTP::Get.new(url.to_s)
             res = Net::HTTP.start(url.host, url.port) {|http|
--- app/controllers/api/icecreams_controller.rb
+++ app/controllers/api/icecreams_controller.rb
@@ -1,11 +1,13 @@
 module Api
     class IcecreamsController < ActionController::API
   
-        URL = "https://cloudmanagement.me"
+        #URL = "http://cloudmanagement.me"
+        URL = "http://localhost"
       
       def index
   
-        path = "#{URL}:11000/#{params[:command]}/#{params[:template]}/#{params[:workspace]}"
+        # path = "#{URL}:11000/#{params[:command]}/#{params[:template]}/#{params[:workspace]}"
+        path = "#{URL}:10000/#{params[:command]}/#{params[:template]}/#{params[:workspace]}"
         url = URI.parse(path)
               req = Net::HTTP::Get.new(url.to_s)
               res = Net::HTTP.start(url.host, url.port) {|http|
