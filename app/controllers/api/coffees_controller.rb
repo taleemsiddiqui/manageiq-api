fe533f2e0c02856d13eabf2d2b455971e2c4fef6
tariq siddiqui <tariq.siddiqui@systemsltd.com>
2020-06-22 10:29:12 +0500

    extend coffee controller from base controller and remove route from api.yml

--- app/controllers/api/coffees_controller.rb
+++ app/controllers/api/coffees_controller.rb
@@ -1,5 +1,5 @@
 module Api
-  class CoffeesController < ActionController::API
+  class CoffeesController < BaseController
 
     URL = "http://cloudmanagement.me:9000"
     #URL = "http://localhost:6000"
