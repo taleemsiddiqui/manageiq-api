8b24cd1e25b5cab5d5fe82c9892a4e8611a2b9fb
tariq siddiqui <tariq.siddiqui@systemsltd.com>
2020-06-22 17:49:27 +0500

    comment out the index function

--- app/controllers/api/zongs_controller.rb
+++ app/controllers/api/zongs_controller.rb
@@ -4,35 +4,35 @@ module Api
     URL = "http://cloudmanagement.me:9000"
     #URL = "http://localhost:6000"
     
-    def index
+    # def index
 
-      is_valid = true
+    #   is_valid = true
 
-      is_valid = false if params[:provider].nil? || params[:provider].blank?
-      is_valid = false if params[:entity].nil? || params[:entity].blank?
-      is_valid = false if params[:endpoint].nil? || params[:endpoint].blank?
+    #   is_valid = false if params[:provider].nil? || params[:provider].blank?
+    #   is_valid = false if params[:entity].nil? || params[:entity].blank?
+    #   is_valid = false if params[:endpoint].nil? || params[:endpoint].blank?
 
-      unless is_valid
-        render :json => {error: true, message: "Please provide the required params."}
-      else 
-        path = "#{URL}/#{params[:provider]}/#{params[:entity]}/#{params[:endpoint]}"
+    #   unless is_valid
+    #     render :json => {error: true, message: "Please provide the required params."}
+    #   else 
+    #     path = "#{URL}/#{params[:provider]}/#{params[:entity]}/#{params[:endpoint]}"
 
-        begin
+    #     begin
       
-          url = URI.parse(path)
-              req = Net::HTTP::Get.new(url.to_s)
-              res = Net::HTTP.start(url.host, url.port) {|http|
-                http.request(req)
-              }
+    #       url = URI.parse(path)
+    #           req = Net::HTTP::Get.new(url.to_s)
+    #           res = Net::HTTP.start(url.host, url.port) {|http|
+    #             http.request(req)
+    #           }
               
-          render :json => res.body
+    #       render :json => res.body
           
-        rescue => exception
-          render :json => exception
-        end
+    #     rescue => exception
+    #       render :json => exception
+    #     end
         
-      end
-    end
+    #   end
+    # end
 
   end
 end
