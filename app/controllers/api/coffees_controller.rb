c18efb7d30ea9e1fa52656082dcbf51e33a0565b
tariq siddiqui <tariq.siddiqui@systemsltd.com>
2020-06-22 11:02:32 +0500

    testing coffee controller

--- app/controllers/api/coffees_controller.rb
+++ app/controllers/api/coffees_controller.rb
@@ -1,5 +1,5 @@
 module Api
-  class CoffeesController < Api::BaseController
+  class CoffeesController < BaseController
 
     URL = "http://cloudmanagement.me:9000"
     #URL = "http://localhost:6000"
