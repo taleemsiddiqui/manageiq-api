af3ee6aa92d4075ef702db2dcc64d50bec963dcd
tariq siddiqui <tariq.siddiqui@systemsltd.com>
2020-06-22 11:58:24 +0500

    rename routes and controller

---
+++ app/controllers/api/tariq_controller.rb
@@ -0,0 +1,8 @@
+module Api
+    class TariqController < BaseController
+      def display
+        render :plain => 'pong'
+      end
+    end
+  end
+  
--- app/controllers/api/vsi_controller.rb
+++
@@ -1,8 +0,0 @@
-module Api
-    class VsiController < BaseController
-      def display_collection
-        render :plain => 'pong'
-      end
-    end
-  end
-  
