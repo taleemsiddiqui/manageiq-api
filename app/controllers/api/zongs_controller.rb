6772f3ae3dfb7e7e91ed47431e1431d3244ad5ab
tariq siddiqui <tariq.siddiqui@systemsltd.com>
2020-06-23 23:36:45 +0500

    refactor code

--- app/controllers/api/zongs_controller.rb
+++ app/controllers/api/zongs_controller.rb
@@ -2,38 +2,69 @@ module Api
   class ZongsController < BaseController
 
     URL = "http://cloudmanagement.me:9000"
+    URL2 = "http://cloudmanagement.me:11000"
     #URL = "http://localhost:6000"
     
-    # def index
+    def coffee
 
-    #   is_valid = true
+      is_valid = true
 
-    #   is_valid = false if params[:provider].nil? || params[:provider].blank?
-    #   is_valid = false if params[:entity].nil? || params[:entity].blank?
-    #   is_valid = false if params[:endpoint].nil? || params[:endpoint].blank?
+      is_valid = false if params[:provider].nil? || params[:provider].blank?
+      is_valid = false if params[:entity].nil? || params[:entity].blank?
+      is_valid = false if params[:endpoint].nil? || params[:endpoint].blank?
 
-    #   unless is_valid
-    #     render :json => {error: true, message: "Please provide the required params."}
-    #   else 
-    #     path = "#{URL}/#{params[:provider]}/#{params[:entity]}/#{params[:endpoint]}"
+      unless is_valid
+        render :json => {error: true, message: "Please provide the required params."}
+      else
+        path = "#{URL}/#{params[:provider]}/#{params[:entity]}/#{params[:endpoint]}"
 
-    #     begin
+        begin
       
-    #       url = URI.parse(path)
-    #           req = Net::HTTP::Get.new(url.to_s)
-    #           res = Net::HTTP.start(url.host, url.port) {|http|
-    #             http.request(req)
-    #           }
+          url = URI.parse(path)
+              req = Net::HTTP::Get.new(url.to_s)
+              res = Net::HTTP.start(url.host, url.port) {|http|
+                http.request(req)
+              }
               
-    #       render :json => res.body
+          render :json => res.body
           
-    #     rescue => exception
-    #       render :json => exception
-    #     end
+        rescue => exception
+          render :json => exception
+        end
         
-    #   end
-    # end
+      end
+    end
 
+    def icecream
+
+      is_valid = true
+
+      is_valid = false if params[:command].nil? || params[:command].blank?
+      is_valid = false if params[:template].nil? || params[:template].blank?
+      is_valid = false if params[:workspace].nil? || params[:workspace].blank?
+
+      unless is_valid
+        render :json => {error: true, message: "Please provide the required params."}
+      else
+        # path = "#{URL}:11000/#{params[:command]}/#{params[:template]}/#{params[:workspace]}"
+        path = "#{URL2}/#{params[:command]}/#{params[:template]}/#{params[:workspace]}"
+
+        begin
+          url = URI.parse(path)
+          req = Net::HTTP::Get.new(url.to_s)
+          res = Net::HTTP.start(url.host, url.port) {|http|
+            http.request(req)
+          }
+
+          render :json => res.body
+        rescue => exception
+          render :json => exception
+        end
+
+
+      end
+
+    end
   end
 end
 
